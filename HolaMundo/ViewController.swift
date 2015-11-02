9924ce9c88dc8ddd635990e32f473af66e6dbec7
Joan Torres <joan.torres.jt@gmail.com>
2015-11-02 18:34:15 -0500

    Initial Commit

---
+++ HolaMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by Joan Torres on 2/11/15.
+//  Copyright © 2015 Joan Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
